7a65520099a82f70b9360173c8896ef97430c662
bhavana <bhavanapoudala9@gmail.com>
2021-03-01 17:58:38 +0530

    Sorted Computation values in Descending order

--- arithematicComputation.sh
+++ arithematicComputation.sh
@@ -10,6 +10,33 @@ Dict[result3]=$(($c+$a/$b))
 Dict[result4]=$(($a%$b+$c))
 
 
-Array=(${Dict[result1]} ${Dict[result2]} ${Dict[result3]} ${Dict[result4]})
-echo "Stored in Array"  ${Array[@]}
+array=(${Dict[result1]} ${Dict[result2]} ${Dict[result3]} ${Dict[result4]})
+echo "Stored in Array"  ${array[@]}
 
+
+echo "Original Numbers in array:"
+for (( i = 0; i <= 4; i++ ))
+do
+  echo ${array[$i]}
+done
+
+
+
+for (( i = 0; i <= 4 ; i++ ))
+do
+   for (( j = $i; j <= 4; j++ ))
+   do
+      if [ ${array[$i]} -lt ${array[$j]}  ]; then
+           t=${array[$i]}
+           array[$i]=${array[$j]}
+           array[$j]=$t
+      fi
+   done
+done
+
+
+echo -e "\nSorted Numbers in Descending Order:"
+for (( i=0; i <= 4; i++ )) 
+do
+  echo ${array[$i]}
+done
