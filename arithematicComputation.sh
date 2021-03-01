4db4af8022b0586e9ade79d355664fbbb48b23a1
bhavana <bhavanapoudala9@gmail.com>
2021-03-01 17:21:45 +0530

    Added: Storing the computation values in a dictionary

--- arithematicComputation.sh
+++ arithematicComputation.sh
@@ -1,8 +1,18 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 
 echo "Enter three inputs :: " 
 read a b c
+declare -A Dict
+Dict[result1]=$(($a+$b*$c))
+Dict[result2]=$(($a*$b+$c))
+Dict[result3]=$(($c+$a/$b))
+Dict[result4]=$(($a%$b+$c))
 
-result=$(($a%$b+$c))
-echo $result
+echo "First computation is: " ${Dict[result1]} 
+echo "Second computation is: " ${Dict[result2]}
+echo "Third computation is: " ${Dict[result3]}
+echo "Fourth computation is: " ${Dict[result4]}
+echo ${Dict[@]}
+
+
